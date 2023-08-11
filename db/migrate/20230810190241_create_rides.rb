f65c204378992f1c8d73d66c335ae8fa0c200188
Tim <mcintyre.tim@gmail.com>
2023-08-10 21:16:16 -0700

    add ride_score to commutes and rename distance_and_duration_updated_at to calculations_updated_at

--- db/migrate/20230810190241_create_rides.rb
+++ db/migrate/20230810190241_create_rides.rb
@@ -6,7 +6,7 @@ class CreateRides < ActiveRecord::Migration[7.0]
       t.decimal :distance, precision: 5, scale: 2
       t.decimal :duration, precision: 4, scale: 2
       t.decimal :earnings, precision: 6, scale: 2
-      t.datetime :distance_and_duration_updated_at
+      t.datetime :calculations_updated_at
       t.timestamps
     end
   end
--- db/migrate/20230810200745_create_commutes.rb
+++ db/migrate/20230810200745_create_commutes.rb
@@ -5,7 +5,8 @@ class CreateCommutes < ActiveRecord::Migration[7.0]
       t.references :ride, null: false, foreign_key: true
       t.decimal :distance, precision: 5, scale: 2
       t.decimal :duration, precision: 4, scale: 2
-      t.datetime :distance_and_duration_updated_at
+      t.decimal :ride_score, precision: 6, scale: 2
+      t.datetime :calculations_updated_at
       t.timestamps
     end
   end
